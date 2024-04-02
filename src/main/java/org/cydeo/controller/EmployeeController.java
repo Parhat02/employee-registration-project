ceda38b977a4801e4c0e2d37a38e64ea5322a351
Ferhat <parhat002@gmail.com>
2024-04-02 21:34:44 +0300

    form-completed

---
+++ src/main/java/org/cydeo/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package org.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @RequestMapping("/register")
+    public String createEmployee(){
+
+        return "employee/employee-create";
+    }
+}
