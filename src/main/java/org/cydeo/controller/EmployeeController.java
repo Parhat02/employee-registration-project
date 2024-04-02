a6194f87e4f7f79aa9d4decf7211114639457ebf
Ferhat <parhat002@gmail.com>
2024-04-02 22:44:58 +0300

    completed

--- src/main/java/org/cydeo/controller/EmployeeController.java
+++ src/main/java/org/cydeo/controller/EmployeeController.java
@@ -2,15 +2,25 @@ package org.cydeo.controller;
 
 import org.cydeo.bootstrap.DataGenerator;
 import org.cydeo.model.Employee;
+import org.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    @RequestMapping("/register")
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/register")
     public String createEmployee(Model model){
 
         model.addAttribute("employee", new Employee());
@@ -18,4 +28,21 @@ public class EmployeeController {
 
         return "employee/employee-create";
     }
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/employee/list";
+    }
+
+    @GetMapping("/list")
+    public String employeeList(Model model){
+
+        model.addAttribute("employeeList",employeeService.readAllEmployees());
+
+        return "employee/employee-list";
+    }
+
 }
---
+++ src/main/java/org/cydeo/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package org.cydeo.service;
+
+import org.cydeo.model.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EmployeeService {
+
+    public static List<Employee> employeeList = new ArrayList<>();
+
+    public void saveEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public List<Employee> readAllEmployees(){
+
+        return employeeList;
+    }
+
+}
