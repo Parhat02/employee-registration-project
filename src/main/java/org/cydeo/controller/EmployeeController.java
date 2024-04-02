10087e615a21c9561a1c187c21e6dbd74f4f680a
Ferhat <parhat002@gmail.com>
2024-04-02 23:12:00 +0300

    Validation

--- src/main/java/org/cydeo/controller/EmployeeController.java
+++ src/main/java/org/cydeo/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package org.cydeo.controller;
 
+import jakarta.validation.Valid;
 import org.cydeo.bootstrap.DataGenerator;
 import org.cydeo.model.Employee;
 import org.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +32,12 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee){
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
 
         employeeService.saveEmployee(employee);
 
--- src/main/java/org/cydeo/model/Employee.java
+++ src/main/java/org/cydeo/model/Employee.java
@@ -1,5 +1,9 @@
 package org.cydeo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -12,16 +16,40 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//    @NotNull -> @NotNull
+//    @NotEmpty -> @NotNull + @NotEmpty
+//    @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String firstName;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String lastName;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String email;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String password;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String address;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String address2;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String city;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String state;
+    @NotEmpty
+    @Size(max = 12, min = 2)
     private String zipCode;
 
 }
